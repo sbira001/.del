4482ddcb272936d768853b72322549fc289d721d
sbira001 <97941692+sbira001@users.noreply.github.com>
2022-01-21 12:48:33 -0800

    Add files via upload

---
+++ server.py
@@ -0,0 +1,16 @@
+import socket
+HOST = '172.16.221.1'  
+PORT = 12121    
+file = open("/data/server_persistent_storage/mydata.txt","r")
+data = file.read()
+
+with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as server:
+    server.bind((HOST, PORT))
+    server.listen()
+    while True:
+        conn, addr = server.accept()
+        print('Connected by', addr)
+        conn.send(data.encode())
+        file.close()
+        print('Closing connection from server')
+        conn.close()
